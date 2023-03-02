db9e3498ad7a68910d603692c2bd649a8dd813fc
WENDPANGA1 <w.ima@alustudent.com>
2023-03-02 01:36:09 -0800

    0-add.py

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/python3
+# Import a simple function from a simple file
+
+def add(a, b):
+    return a + b
+
+from my_functions import add_numbers
+result = add_numbers(1, 2)
+print(result) #output: 3
