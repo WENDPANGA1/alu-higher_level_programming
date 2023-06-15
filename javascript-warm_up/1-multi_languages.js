5730a674a0a0436f8e13d76fc380210a0790e7d4
WENDPANGA1 <w.ima@alustudent.com>
2023-06-16 01:03:15 +0300

    java

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -4,6 +4,6 @@ const iL = "C is fun";
 const iLl = "Python is cool";
 const iLll = "JavaScript is amazing";
 
-console.log (iL)
-console.log(iLl)
-console.log(iLll)
+console.log (iL);
+console.log(iLl);
+console.log(iLll);
